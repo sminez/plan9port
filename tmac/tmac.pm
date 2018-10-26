a47e522bef4f428e8e37cc643f6438012909a356
sminez <innesdmorrison@gmail.com>
2018-10-26 09:23:14 +0100

    fixing shebang lines after local directory move

--- tmac/tmac.pm
+++ tmac/tmac.pm
@@ -1,5 +1,5 @@
 .\" 10/22/92 activate next line before installing
-.pi /home/innes/github/plan9/bin/auxpm
+.pi /home/innes/Personal/plan9/bin/auxpm
 .
 .		\" IZ - initialization
 .de IZ
@@ -963,4 +963,4 @@ Naperville, Illinois 60540
 ..
 .IZ
 .rm IZ
-.so /home/innes/github/plan9/tmac/tmac.srefs
+.so /home/innes/Personal/plan9/tmac/tmac.srefs
